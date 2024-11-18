9d2e5a80fda36ed4bdb012c0ab9a8fc4e398ba2f
jsteel17 <161091774+jsteel17@users.noreply.github.com>
2024-11-18 19:47:50 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  let whoChoice = Math.floor(Math.random() * who.length);
+  let actionChoice = Math.floor(Math.random() * action.length);
+  let whatChoice = Math.floor(Math.random() * what.length);
+  let whenChoice = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[whoChoice] + action[actionChoice] + what[whatChoice] + when[whenChoice];
 };
